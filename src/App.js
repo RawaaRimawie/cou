afe179b45d61819c61456d1a661ec47bffb86825
Rawarimaei <125211778+Rawarimaei@users.noreply.github.com>
2024-09-26 10:44:38 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useSelector, useDispatch } from 'react-redux'; // Corrected here
+import { increment, decrement } from './redux/counterSlice';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const counter = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch(); // Fixed the variable name
+
+    return (
+        <div>
+            <button onClick={() => dispatch(increment())}>Increment +</button>
+            <h1>Count: {counter}</h1>
+            <button onClick={() => dispatch(decrement())}>Decrement -</button>
+        </div>
+    );
 }
 
 export default App;
